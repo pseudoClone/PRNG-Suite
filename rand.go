cd3419d453f7a18358ede8cc4d2725e2a328b097
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-09 07:44:15 +0545

    new: LCG using big numbers Use of constant seed, a better implementation would be using UNIX epoch time for seed. Also, could use a range for limited output

---
+++ rand.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+)
+
+func main() {
+	xn := new(big.Int).SetInt64(0) //Will set later
+	m := new(big.Int).SetInt64((int64(math.Pow(2, 31) - 1))) // Ideally prime
+	a := big.NewInt(67280421310721) // Biggest prime by Thomas Clausen
+	c := 2531011 // Wiki Parameters
+
+	for i := 0;; i++ {
+		// xn = (a * xn + c) % m (Never do big integer arithmetic like this)
+		xn = new(big.Int).Mul(a, xn)
+		xn = new(big.Int).Add(xn, big.NewInt(int64(c)))
+		xn = new(big.Int).Mod(xn, m)
+		fmt.Printf("%d   ->  %d\n",i,xn)
+	}
+}
